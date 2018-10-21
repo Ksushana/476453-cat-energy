b07e91d49a1df899f5755b9c35fbe2fefd62a089
Ksenia Balashova <utyatka@gmail.com>
2018-10-21 12:36:00 +0200

    ie fixed

--- source/js/slider-index.js
+++ source/js/slider-index.js
@@ -2,20 +2,20 @@ var sliderControlFat = document.querySelector(".slider-control--fat");
 var sliderControlSlim = document.querySelector(".slider-control--slim");
 var slideFat = document.querySelector(".slider-slide--fat");
 var slideSlim = document.querySelector(".slider-slide--slim");
-var sliderMark = document.querySelector(".slider-mark");
+var sliderValue = document.querySelector(".slider-range-value");
 
 var changeOnFat = function () {
   slideSlim.classList.add("visually-hidden");
   slideFat.classList.remove("visually-hidden");
-  sliderMark.classList.add("slider-mark--fat");
-  sliderMark.classList.remove("slider-mark--slim");
+  sliderValue.classList.add("slider-range-value--fat");
+  sliderValue.classList.remove("slider-range-value--slim");
 }
 
 var changeOnSlim = function () {
   slideSlim.classList.remove("visually-hidden");
   slideFat.classList.add("visually-hidden");
-  sliderMark.classList.add("slider-mark--slim");
-  sliderMark.classList.remove("slider-mark--fat");
+  sliderValue.classList.add("slider-range-value--slim");
+  sliderValue.classList.remove("slider-range-value--fat");
 }
 
 
@@ -27,6 +27,5 @@ var onSlimClick = function () {
   changeOnSlim();
 }
 
-// console.log('sliderControlFat', document.querySelector(".slider-control--fat"));
 sliderControlFat.addEventListener("click", onFatClick);
 sliderControlSlim.addEventListener("click", onSlimClick);
