2174eeb1e8cdb9a9c6b87339f7c700f4f95b2c0c
Ksenia Balashova <utyatka@gmail.com>
2018-10-23 23:32:37 +0200

    first error correction

--- source/js/slider-index.js
+++ source/js/slider-index.js
@@ -1,21 +1,21 @@
-var sliderControlFat = document.querySelector(".slider-control--fat");
-var sliderControlSlim = document.querySelector(".slider-control--slim");
-var slideFat = document.querySelector(".slider-slide--fat");
-var slideSlim = document.querySelector(".slider-slide--slim");
-var sliderValue = document.querySelector(".slider-range-value");
+var sliderControlFat = document.querySelector(".slider__control--fat");
+var sliderControlSlim = document.querySelector(".slider__control--slim");
+var slideFat = document.querySelector(".slider__slide--fat");
+var slideSlim = document.querySelector(".slider__slide--slim");
+var sliderValue = document.querySelector(".slider__range-value");
 
 var changeOnFat = function () {
   slideSlim.classList.add("visually-hidden");
   slideFat.classList.remove("visually-hidden");
-  sliderValue.classList.add("slider-range-value--fat");
-  sliderValue.classList.remove("slider-range-value--slim");
+  sliderValue.classList.add("slider__range-value--fat");
+  sliderValue.classList.remove("slider__range-value--slim");
 }
 
 var changeOnSlim = function () {
   slideSlim.classList.remove("visually-hidden");
   slideFat.classList.add("visually-hidden");
-  sliderValue.classList.add("slider-range-value--slim");
-  sliderValue.classList.remove("slider-range-value--fat");
+  sliderValue.classList.add("slider__range-value--slim");
+  sliderValue.classList.remove("slider__range-value--fat");
 }
 
 
